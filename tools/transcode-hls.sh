748c6728b797839fdc02451d2c6a79ddb94d4810
tamno <takey@hotmail.com>
2026-05-30 23:26:48 +0800

    update

--- tools/transcode-hls.sh
+++ tools/transcode-hls.sh
@@ -13,10 +13,10 @@ mkdir -p "$output_dir"/{720p,480p,360p}
 
 ffmpeg -y -i "$input" \
   -filter_complex \
-  "[0:v]split=3[v720][v480][v360];[v720]scale=w=1280:h=-2[v720out];[v480]scale=w=854:h=-2[v480out];[v360]scale=w=640:h=-2[v360out]" \
-  -map "[v720out]" -map 0:a:0 -c:v:0 libx264 -preset medium -crf 23 -maxrate:v:0 2800k -bufsize:v:0 5600k -c:a:0 aac -b:a:0 128k \
-  -map "[v480out]" -map 0:a:0 -c:v:1 libx264 -preset medium -crf 24 -maxrate:v:1 1400k -bufsize:v:1 2800k -c:a:1 aac -b:a:1 96k \
-  -map "[v360out]" -map 0:a:0 -c:v:2 libx264 -preset medium -crf 25 -maxrate:v:2 800k -bufsize:v:2 1600k -c:a:2 aac -b:a:2 96k \
+  "[0:v]fps=25,split=3[v720][v480][v360];[v720]scale=w=1280:h=-2[v720out];[v480]scale=w=854:h=-2[v480out];[v360]scale=w=640:h=-2[v360out]" \
+  -map "[v720out]" -map 0:a:0 -c:v:0 libx264 -preset medium -crf 23 -profile:v:0 main -level:v:0 4.0 -pix_fmt:v:0 yuv420p -g:v:0 50 -keyint_min:v:0 50 -sc_threshold:v:0 0 -maxrate:v:0 2800k -bufsize:v:0 5600k -c:a:0 aac -ar:a:0 48000 -ac:a:0 2 -b:a:0 128k \
+  -map "[v480out]" -map 0:a:0 -c:v:1 libx264 -preset medium -crf 24 -profile:v:1 main -level:v:1 3.1 -pix_fmt:v:1 yuv420p -g:v:1 50 -keyint_min:v:1 50 -sc_threshold:v:1 0 -maxrate:v:1 1400k -bufsize:v:1 2800k -c:a:1 aac -ar:a:1 48000 -ac:a:1 2 -b:a:1 96k \
+  -map "[v360out]" -map 0:a:0 -c:v:2 libx264 -preset medium -crf 25 -profile:v:2 main -level:v:2 3.0 -pix_fmt:v:2 yuv420p -g:v:2 50 -keyint_min:v:2 50 -sc_threshold:v:2 0 -maxrate:v:2 800k -bufsize:v:2 1600k -c:a:2 aac -ar:a:2 48000 -ac:a:2 2 -b:a:2 96k \
   -f hls \
   -hls_time 6 \
   -hls_playlist_type vod \
